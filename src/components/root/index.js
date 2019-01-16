8ea9d41ac55bda1b114da2eb1cf85e7e619b6b48
Sergio Zybin <serdukeukr@gmail.com>
2019-01-16 13:45:23 +0200

    Add on the library react material into the project

--- src/components/root/index.js
+++ src/components/root/index.js
@@ -14,7 +14,7 @@ import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from '../menu';
+import { mainListItems, secondaryListItems } from '../Menu';
 import SimpleTable from '../SimpleTable';
 
 const drawerWidth = 240;
